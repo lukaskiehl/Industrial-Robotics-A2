304996b078a25a0dde646d93af5c44baf154d655
TejasB4 <145757215+TejasB4@users.noreply.github.com>
2023-10-06 15:57:35 +1100

    Add files via upload

--- RunnerLab2/RunnerLab2.m
+++ RunnerLab2/RunnerLab2.m
@@ -4,68 +4,75 @@ function RunnerLab2
 
 % Run Code
 %Load Environment
-clf;
-close all;
+close all hidden
+clear all
+clear classes
+close all
+clc
 balls = RobotBalls;
 KR = KR6R700CR;
 steps = 30;
-KR.model.teach();
-
-
-input('press enter to pickup ball')
+UR3StartPos = transl(-5,0.5,0)*trotz(-pi/2);
+UR3 = UR3Batter(UR3StartPos);
+ballTransl = transl(-0.2,0,-0);
 
 %1.0 Pick up ball
 qpasser1 = [ 0    2.5598    0.4145         0         0         0];
-q1 = zeros(1,6);
-q2 = KR.model.ikcon(balls.ballModel{1}.base, qpasser1);
+krq1 = zeros(1,6);
+krq2 = KR.model.ikcon(balls.ballModel{1}.base, qpasser1);
 s = lspb(0,1,steps); % use trapezoidal velocity method from Lab 4.1
 qMatrix = nan(steps,6);
      for i = 1:steps
-        qMatrix(i,:) = (1-s(i))*q1 + s(i)*q2;
+        qMatrix(i,:) = (1-s(i))*krq1 + s(i)*krq2;
      end
 for i = 1:steps
         KR.model.animate(qMatrix(i,:));
-        %pause(0.2);
         drawnow();
 end
 % 1.1 Prepare to throw the ball
-q1 = q2;
-q2 = [1.2363    0.0818   -2.0944    0.0000   -0.0122    0.0000];
+krq1 = krq2;
+krq2 = [pi    0.0818   -2.0944    0.0000   -0.0122    0.0000];
 s = lspb(0,1,steps); % use trapezoidal velocity method from Lab 4.1
 qMatrix = nan(steps,6);
      for i = 1:steps
-        qMatrix(i,:) = (1-s(i))*q1 + s(i)*q2;
+        qMatrix(i,:) = (1-s(i))*krq1 + s(i)*krq2;
      end
 for i = 1:steps
         KR.model.animate(qMatrix(i,:));
         balls.ballModel{1}.base = KR.model.fkine(qMatrix(i,:));
         balls.ballModel{1}.animate(0);
-        %pause(0.2);
         drawnow();
 end
 % 1.2 Action to throw the ball
-q1 = q2;
-q2 = [ 1.2363    2.8162   -0.7898         0   -0.0122   -1.5272];
-s = lspb(0,1,steps/2); % use trapezoidal velocity method from Lab 4.1
-qMatrix = nan(steps/2,6);
-     for i = 1:steps/2
-        qMatrix(i,:) = (1-s(i))*q1 + s(i)*q2;
+krq1 = krq2;
+krq2 = [ pi    2.8162   -0.7898         0   -0.0122   -1.5272];
+s = lspb(0,1,steps); % use trapezoidal velocity method from Lab 4.1
+qMatrix = nan(steps,6);
+     for i = 1:steps
+        qMatrix(i,:) = (1-s(i))*krq1 + s(i)*krq2;
      end
-for i = 1:steps/2
+for i = 1:steps
         KR.model.animate(qMatrix(i,:));
-        %pause(0.2); 
 
-        if steps/4 >= i
+        if steps/1.36 >= i
           balls.ballModel{1}.base = KR.model.fkine(qMatrix(i,:));
           balls.ballModel{1}.animate(0);
           drawnow();
-          if i == steps/4
+          if i == steps/1.36
               ballStart = KR.model.fkine(qMatrix(i,:))
           end
         end
-        if i > steps/4
-           % for j = 0:0.01:0.2
-                  balls.ballModel{1}.base = balls.ballModel{1}.base.T*transl(.05,0.1,0);
+        if i > steps/1.36
+           % for j = 0:0.01:0.1
+                  % reset orientation of ball so we can control it. Can do
+                  % in a function later
+                  ballPos = balls.ballModel{1}.base.T;
+                  newBallPos = eye(4);
+                  newBallPos(1:3,4) = ballPos(1:3,4);
+                  balls.ballModel{1}.base = newBallPos;
+
+                  % Animate it
+                  balls.ballModel{1}.base = balls.ballModel{1}.base.T*ballTransl;
                   balls.ballModel{1}.animate(0);
                   drawnow();
            % end
@@ -74,11 +81,31 @@ for i = 1:steps/2
         drawnow();
 
 end
+for i = 1:steps
+    ballXYZ = balls.ballModel{1}.base.T;
+    
+    ballXYZ2 = balls.ballModel{1}.base.T;
+    ballXYZ2(1,4) = -5;
 
-%% Collision check 
+    urq1 = UR3.model.getpos();
+    urq2 = UR3.model.ikcon(ballXYZ2*trotx(pi/2)*trotz(pi/2));
 
+    s = lspb(0,1,steps); % use trapezoidal velocity method from Lab 4.1
+    qMatrix = nan(steps,6);
+    for i = 1:steps
+            qMatrix(i,:) = (1-s(i))*urq1 + s(i)*urq2;
+    end
+    
+    for i = 1:steps
+            UR3.model.animate(qMatrix(i,:));
+            drawnow();
+            balls.ballModel{1}.base = balls.ballModel{1}.base.T*ballTransl;
+            balls.ballModel{1}.animate(0);
+            drawnow();
+    end
+end
 
-
-
+    
+    
 
 
